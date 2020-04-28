11a3ade2516fee456dc53d84ccd87c6c3eccc409
LAEEQ18 <starsleo18@gmail.com>
2020-04-29 04:58:08 +0500

    Updates

---
+++ precache-manifest.2cfec22f2168d691d46e5ff36238213f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d9e6efdb9854aa3e3c603bec02ce5e",
+    "url": "/monster-robo/index.html"
+  },
+  {
+    "revision": "c6be4d787094d20bcbc7",
+    "url": "/monster-robo/static/css/main.70db4d79.chunk.css"
+  },
+  {
+    "revision": "83708b8d3de79eae6b22",
+    "url": "/monster-robo/static/js/2.833f470a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-robo/static/js/2.833f470a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6be4d787094d20bcbc7",
+    "url": "/monster-robo/static/js/main.edf0f55f.chunk.js"
+  },
+  {
+    "revision": "3f722729b2d8ae37cc5f",
+    "url": "/monster-robo/static/js/runtime-main.ef00653b.js"
+  }
+]);
